1ed2497de4c4b30af529bcf7cf00054602d74e0c
Intaveon <jeroen161098@gmail.com>
2024-09-01 10:25:14 +0200

    vault backup: 2024-09-01 10:25:13

---
+++ content/Courses/Hydrogeology.md
@@ -0,0 +1,19 @@
+---
+fileClass: Base, Courses
+title: 
+Parents: ["[[index]]"]
+Draft: 
+Status: In Progress
+Course Code: HWM20806
+Period: 1
+Duration: Whole Period
+Timeslot: Morning
+Year: 2024
+---
+
+# 🔎 Introduction
+The course presents an overview of the role of soil and water in the landscape. Central topic is the diversity and coherence in space and time, especially the factors that determine the role of water for human use and nature.
+
+# 📖 Course Material
+The literature for this course consists of a reader which has been divided into 3 parts
+
--- content/Scripts/newNote.js
+++ content/Scripts/newNote.js
@@ -15,7 +15,7 @@ async function newNote() {
         (await tp.file.move("/Media/" + tp.file.title));
     } else {
         // Suggester for course selection
-        course = await tp.system.suggester(["Water 1", "Water 2", "Mathematics 2", "Mathematics 3", "General Physics", "Water Quantity and Quality"], ["Water 1", "Water 2", "Mathematics 2", "Mathematics 3", "General Physics", "Water Quantity and Quality"], true);
+        course = await tp.system.suggester(["Multivariate Mathematics Applied", "Hydrogeology", "Water 1", "Water 2", "Mathematics 2", "Mathematics 3", "Water Quantity and Quality"], ["Multivariate Mathematics Applied", "Hydrogeology", "Water 1", "Water 2", "Mathematics 2", "Mathematics 3", "Water Quantity and Quality"], true);
 
         // Find note type base
         noteType = await tp.system.suggester(["Courses", "Notes", "Terminology", "Questions"], ["Courses", "Notes", "Terminology", "Questions"], true);
